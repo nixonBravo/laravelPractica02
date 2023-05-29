e41f9760e1428fc15d893e552a66a48d5bc3e838
nixonBravo <nixonelian2002@gmail.com>
2023-05-28 23:59:45 -0500

    FrutaController Terminado

--- app/Http/Controllers/FrutaController.php
+++ app/Http/Controllers/FrutaController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fruta;
 use Illuminate\Http\Request;
 
 class FrutaController extends Controller
 {
     
+    public function index(){
+        return view('frutas.insertar');
+    }
+    
+    public function insertar(Request $request)
+    {
+        $frutas = new Fruta();
+        $frutas->nombre = $request->nombre;
+        $frutas->precio = $request->precio;
+        $frutas->cantidad = $request->cantidad;
+        $frutas->save();
+        return view('frutas.insertar');
+    }
+
+    public function obtener(){
+        $frutas = Fruta::all();
+        return view('frutas.obtener', compact('frutas'));
+    }
+
 }
--- resources/views/frutas/insertar.blade.php
+++ resources/views/frutas/insertar.blade.php
@@ -4,7 +4,7 @@
  
     <div class="position-absolute top-50 start-50 w-50 translate-middle">
         <div class="bg-info p-2">
-            <h2 class="text-center mb-5">Formulario Crear Vehiculo</h2>
+            <h2 class="text-center text-danger mt-3 mb-5">Formulario Insertar Fruta</h2>
             <form action="{{url('insertarFruta')}}" method="POST" >
                 @csrf
                 <div class="mb-3">
@@ -16,7 +16,7 @@
                 <div class="mb-3">
                     <input type="text" class="form-control" name="cantidad" placeholder="Cantidad">
                 </div>
-                <div class="d-flex justify-content-center">
+                <div class="d-flex justify-content-center mb-3">
                     <button type="submit" class="btn btn-primary">Crear</button>
                 </div>
             </form>
